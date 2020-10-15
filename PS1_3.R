39b1c42978b366d894956f5ed5bc97c33ac08783
MingYang-Hydrate <71815635+MingYang-Hydrate@users.noreply.github.com>
2020-10-15 18:37:26 +0800

    Update PS1_3.R
    add some advice for your code

--- PS1_3.R
+++ PS1_3.R
@@ -3,6 +3,7 @@ Pascal_triangle <- function(k) {
   data_k_line[1] <- 1
   m <- 1
   for (i in 2:k) {
+    #应加个判断，当k=1时如何处理？事实上，当k为1时你的代码无法输出正确的结果
     m <- m*((k-i+1)/(i-1))
     data_k_line[i] <- m
   }
