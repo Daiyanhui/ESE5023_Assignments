fdef1d1b4b2ec30b43453ae37c9df0a54341e2df
MingYang-Hydrate <71815635+MingYang-Hydrate@users.noreply.github.com>
2020-10-15 17:40:24 +0800

    Update PS1_2.R
    using "<-" instead of "=", they have differences in some condition(to avoid unpredictable bugs)

--- PS1_2.R
+++ PS1_2.R
@@ -3,7 +3,7 @@ M2 <- matrix(sample(0:50,50),nrow = 10,ncol = 5)
 M3 <- matrix(nrow=5,ncol=5)
 Matrix_multip <- function(m1,m2) {
   c=M1%*%M2
-  
+  # c<-M1%*M2
   for (i in 1:5){
     for (j in 1:5){
       m <- 1
@@ -11,7 +11,9 @@ Matrix_multip <- function(m1,m2) {
       for (m in 1:10){
         
         sum=M1[i,m]*M2[m,j]+sum
+        # sum<-M1[i,m]*M2[m,j]+sum
         M3[i,j]=sum
+        #M3[i,j]<-sum
       }
     }
   }
