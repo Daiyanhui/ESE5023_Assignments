f99ef1d79b1eab5b1935682023eacc70e9f2e4e7
MingYang-Hydrate <71815635+MingYang-Hydrate@users.noreply.github.com>
2020-11-10 22:48:38 +0800

    Update PS2_2.R
    some suggestions for you to select the qualified data briefly

--- PS2_2.R
+++ PS2_2.R
@@ -19,3 +19,22 @@ data_valid %>%
   geom_line() +
   facet_wrap(~ Month)
   
+# @MingYANG noticed:
+# to select the qualified data, function "filter" should be used properly
+# try and understand this:
+# a_Data       <- read.csv(file = "2281305.csv", header = T)
+# a_Data <- as_tibble(a_Data)
+# a_Data%>% 
+#   select(WND,DATE) %>% 
+#   filter(substr(WND,1,3)!="999") %>% 
+#   filter(substr(WND,9,12)!="9999") %>% 
+#   filter(substr(WND,5,7)=="1,N")%>%
+#   filter(substr(WND,14,14)=="1")%>%
+#   mutate(Month=as.character(paste0(substr(DATE,1,4),"-",substr(DATE,6,7)))) %>%
+#   mutate(wind_speed=as.numeric(substr(WND,9,12)))%>%
+#   select(Month,wind_speed) %>% 
+#   group_by(Month) %>% 
+#   summarise(windspeed_month=mean(wind_speed,na.rm = T)) %>% 
+#   mutate(month = as.Date(paste0(Month,"-","15"))) %>%
+#   ggplot(aes(x=month, y=windspeed_month)) + 
+#  geom_line()
