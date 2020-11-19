532f31d427434978e7cc29d7e142c95a0e053c23
Yanhui Dai <71826808+Daiyanhui@users.noreply.github.com>
2020-11-19 18:43:11 +0800

    Add files via upload

---
+++ PS4_1.R
@@ -0,0 +1,77 @@
+library(tidyverse)
+library(ggpubr) 
+theme_set(
+  theme_bw() + 
+    theme(legend.position = "top")
+)
+
+
+#1.1 scatter plot
+library("ggpubr")
+p <- ggplot(mtcars, aes(mpg, wt)) +
+  geom_point() +
+  geom_smooth(method = lm) +
+  stat_cor(method = "pearson", label.x = 20)
+p
+
+#use library(ggforce)
+library(ggforce)
+ggplot(iris, aes(Petal.Length, Petal.Width, colour = Species)) +
+  geom_point() +
+  facet_zoom(x = Species == "versicolor")
+
+
+# Basic scatter plot
+ggplot(mpg, aes(cty, hwy)) +
+  geom_point(size = 0.5)
+
+# Jittered points
+ggplot(mpg, aes(cty, hwy)) +
+  geom_jitter(size = 0.5, width = 0.5)
+
+
+# 1.2 box plot
+# Use box plot as marginal plots
+ggscatterhist(
+  iris, x = "Sepal.Length", y = "Sepal.Width",
+  color = "Species", size = 3, alpha = 0.6,
+  palette = c("#00AFBB", "#E7B800", "#FC4E07"),
+  margin.plot = "boxplot",
+  ggtheme = theme_bw()
+)
+
+
+
+#1.3 Histogram
+ggplot(iris, aes(Sepal.Length)) +
+  geom_histogram(aes(fill = Species, color = Species), bins = 20, 
+                 position = "identity", alpha = 0.5) +
+  scale_fill_viridis_d() +
+  scale_color_viridis_d()
+
+
+# 1.4 Time series
+library(ggplot2)
+library(dplyr)
+
+# Dummy data
+data <- data.frame(
+  day = as.Date("2017-06-14") - 0:364,
+  value = runif(365) + seq(-140, 224)^2 / 10000
+)
+
+# Most basic bubble plot
+p <- ggplot(data, aes(x=day, y=value)) +
+  geom_line() + 
+  xlab("")
+p
+
+
+
+# 1.5 image plot
+#Yiwen introduce this method to me,and I learned the code from him
+sample = cor(matrix(rnorm(400),nrow = 20))
+image(cor(matrix(rnorm(400),nrow = 20)),axes = F)
+mtext(text = c(paste("country",1:21)),side = 2,line = 0.3,at = seq(0,1,0.05),las = 1,cex = 0.8)
+mtext(text = c(paste("country",1:21)),side = 1,line = 0.3,at = seq(0,1,0.05),las = 2,cex = 0.8)
+image.plot(sample,legend.only = T)
---
+++ PS4_2.R
@@ -0,0 +1,76 @@
+#The parts of Time Series was inspired by TaoTaoSun and Li Yuan
+#I learned the code from Li
+#This part,I haven't grasp it.
+
+library(dplyr)
+library(lubridate)
+library(forecast)
+
+data_tbl <- as_tibble(read.csv(file = "2281305.csv", header = T))
+
+data_tbl <- data_tbl %>%
+  select(DATE,TMP) %>%
+  mutate(
+    T_value = as.numeric(substr(TMP,1,5)),
+    T_flag = as.logical(as.numeric(substr(TMP,7,7)))
+  ) %>% 
+  
+  filter(T_value!= 9999 & T_flag==TRUE) %>% 
+  mutate(T_value2 = T_value * 0.1,
+         Obs_Time = as.Date(DATE))
+
+data_tbl <- data_tbl %>%
+  select(Obs_Time,T_value2) %>%
+  group_by(Obs_Time) %>%
+  summarise(T_value_monthly = mean(T_value2)) 
+
+plot(data_tbl$Obs_Time,data_tbl$T_value_monthly, 
+     type="l",xlab="Date",ylab="BaoAn_T_value")
+
+#2.1 time series from 2010 Jan. to 2020 Aug.
+monthly_averaged_temp_ts <- ts(data_tbl$T_value_monthly, 
+                               start=c(2010,1), end=c(2020,8),frequency=12)
+# Check structure
+str(monthly_averaged_temp_ts)
+# Plot time series
+plot(monthly_averaged_temp_ts, type="l")
+
+#2.2 Decompose the time series into trend, seasonality, 
+#and error parts. 
+#-------------------------------------------------
+time_series_components <- decompose(monthly_averaged_temp_ts)
+plot(time_series_components)
+
+hist(time_series_components$random, prob=TRUE)
+
+curve(dnorm(x, mean=mean(time_series_components$random,
+                         na.rm=T),sd=sd(time_series_components$random,na.rm=T)),
+      add=TRUE, col="green")
+
+
+#2.3 Fit an ARIMA(p,d,q) model to the time series. 
+trModel <- lm(monthly_averaged_temp_ts ~ c(1:length(monthly_averaged_temp_ts)))
+plot(resid(trModel), type="l")
+
+acf(resid(trModel))
+pacf(resid(trModel))
+
+auto.arima(monthly_averaged_temp_ts,trace=T)
+
+arima1 <- arima(monthly_averaged_temp_ts,order=c(1,0,0),seasonal=list(order=c(1,0,0),period=12),method="ML")
+arima1
+arima2 <- arima(monthly_averaged_temp_ts,order=c(1,0,0),seasonal=list(order=c(2,1,0),period=12),method="ML")
+arima2
+
+
+#2.4 Predict monthly-averaged temperatures in 2020 Sep. and Oct
+model <- arima2
+months_forecast  <- 2
+months_in_plot <- 20
+forecast_2months <- forecast(model, months_forecast)
+
+# Plot predictions along with real values
+plot(forecast(model, months_forecast), include = months_in_plot, 
+     xlab="Time", 
+     ylab="Temperature",type="o",lwd=1,
+     col = "blue") 
