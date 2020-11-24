0ad83376aa5430e0ab70ae6a26d00cbffb08dbf3
MingYang-Hydrate <71815635+MingYang-Hydrate@users.noreply.github.com>
2020-11-25 00:23:26 +0800

    Update PS3_5.R
    writing something better than none

--- PS3_5.R
+++ PS3_5.R
@@ -15,3 +15,10 @@ fit1 <- lm(Distance ~ Velocity, data=data_tbl)
 summary(fit1)
 abline(fit1,lwd=5,col="blue")
 #5.3
+# MingYANG noticed：
+fit_new<-lm(Distance-0.399098216 ~Velocity,data=hubble_data_tbl)
+summary(fit_new)
+abline(fit_new, lwd = 5, col = "black")
+coef(fit_new)
+age<-(fit_new$coefficients[2]*30.9/60/60/24/365)
+# the end
